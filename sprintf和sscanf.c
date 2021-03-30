a80f17b9d23f31f6211a323fb20c01b59860a9b8
sforn-null <1641832926@qq.com>
2021-03-30 22:45:06 +0800

    sprintf和sscanf

---
+++ sprintf和sscanf.c
@@ -0,0 +1,22 @@
+#include<stdio.h>.
+
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//	float score;
+//};
+//
+//int main()
+//{
+//	struct Stu S = { "zhangsan",19,66.5f };
+//	char str[100] = { 0 };
+//	struct Stu T = { 0 };
+//	sprintf(str, "%s %d %f", S.name, S.age, S.score);
+//	//将一个标准格式转化为字符串
+//	printf("%s", str);
+//	sscanf = (str, "%s %d %f", T.name, &(T.age), &(T.score));
+//	//将字符串转化为标准格式
+//	printf("%s %d %f", T.name, T.age, T.score);
+//	return 0;
+//}
